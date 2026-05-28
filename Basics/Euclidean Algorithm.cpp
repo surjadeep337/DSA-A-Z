5bbbf040b2506599532d50f352345bcd33a7096a
surjadeep337 <surjadas337@gmail.com>
2026-05-28 14:20:32 +0530

    Add Euclidean Algorithm

---
+++ Basics/Euclidean Algorithm.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+
+    while(b != 0) {
+
+        int remainder = a % b;
+
+        a = b;
+
+        b = remainder;
+    }
+
+    return a;
+}
+
+int main() {
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << gcd(a, b);
+
+    return 0;
+}
